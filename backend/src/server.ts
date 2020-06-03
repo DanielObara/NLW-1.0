b78e6856a2fe3da767de21b35f4f82466f928090
Daniel Obara <daniel.tsutomu@hotmail.com>
2020-06-03 00:09:33 -0300

    Feat/Just a improvment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,4 +11,6 @@ app.use(routes);
 
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
-app.listen(3333);
+app.listen(3333, () => {
+	console.log("🚨️ Bug-end started! To de brinks ;D");
+});
